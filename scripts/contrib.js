9fdcccd952c25199a19d18b84a96189014636398
Restyled.io <commits@restyled.io>
2024-05-15 02:47:55 +0000

    Restyled by prettier

--- scripts/contrib.js
+++ scripts/contrib.js
@@ -1,20 +1,19 @@
+import format from "format-package-json";
+import { GitContributors } from "git-contributors";
+import { join } from "path";
 
-import format from 'format-package-json';
-import {GitContributors} from 'git-contributors';
-import {join} from 'path';
-
-const opts = join(__dirname, '../');
-const pkg = join(__dirname, '../package.json');
+const opts = join(__dirname, "../");
+const pkg = join(__dirname, "../package.json");
 const json = require(pkg);
 
 json.contributors = []; // clear it
 
-GitContributors.list(opts, function(err, result) {
-  result.forEach(function(item) {
-    json.contributors.push([ item.name, '<' + item.email + '>' ].join(' '));
+GitContributors.list(opts, function (err, result) {
+  result.forEach(function (item) {
+    json.contributors.push([item.name, "<" + item.email + ">"].join(" "));
   });
   json.contributors.sort();
-  format(pkg, json, function() {
-    console.log('Wrote %s contributors to: %s', result.length, pkg);
+  format(pkg, json, function () {
+    console.log("Wrote %s contributors to: %s", result.length, pkg);
   });
 });

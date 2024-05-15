e5e286fd41c0ad022f52f1ae09f2efce1756f388
Restyled.io <commits@restyled.io>
2024-05-15 02:47:51 +0000

    Restyled by clang-format

--- scripts/contrib.js
+++ scripts/contrib.js
@@ -1,23 +1,20 @@
 
-import { join } from 'path';
 import format from 'format-package-json';
-import { GitContributors } from 'git-contributors';
+import {GitContributors} from 'git-contributors';
+import {join} from 'path';
+
 const opts = join(__dirname, '../');
 const pkg = join(__dirname, '../package.json');
 const json = require(pkg);
- 
-json.contributors = []; //clear it
+
+json.contributors = []; // clear it
 
 GitContributors.list(opts, function(err, result) {
-    result.forEach(function(item) {
-        json.contributors.push([item.name, '<' + item.email + '>'].join(' '));
-    });
-    json.contributors.sort();
-    format(pkg, json, function() {
-        console.log('Wrote %s contributors to: %s', result.length, pkg);
-    });
+  result.forEach(function(item) {
+    json.contributors.push([ item.name, '<' + item.email + '>' ].join(' '));
+  });
+  json.contributors.sort();
+  format(pkg, json, function() {
+    console.log('Wrote %s contributors to: %s', result.length, pkg);
+  });
 });
-
-
-
-

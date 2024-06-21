0ddba18a934fcec376214d512883687d60719454
Ciaran Welch <bubonicfred@gmail.com>
2024-06-21 16:27:28 +1000

    test: refactor of tests
    refactor: replace builtin prototype method with Object.prototype equivalent

--- tests/test.js
+++ tests/test.js
@@ -543,7 +543,7 @@ describe('main tests', function() {
                     publisher: false
                 }
             }, function(err, output) {
-                assert(output.hasOwnProperty('abbrev@1.0.9'), 'Check if the expected package still exists.');
+                assert(Object.prototype.hasOwnProperty.call(output, 'abbrev@1.0.9'), 'Check if the expected package still exists.');
                 assert.equal(output['abbrev@1.0.9'].copyright, 'Copyright (c) Isaac Z. Schlueter and Contributors');
                 done();
             });

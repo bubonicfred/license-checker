276d6df4b21e35642012a4d45c6628366180c533
Ciaran Welch <bubonicfred@gmail.com>
2024-06-21 20:27:10 +1000

    refactor(ES2015): Convert functions to arrow functions

--- scripts/contrib.js
+++ scripts/contrib.js
@@ -9,12 +9,12 @@ const json = require(pkg);
 
 json.contributors = []; //clear it
 
-GitContributors.list(opts, function(err, result) {
-    result.forEach(function(item) {
+GitContributors.list(opts, (err, result) => {
+    result.forEach((item) => {
         json.contributors.push([item.name, `<${item.email}>`].join(' '));
     });
     json.contributors.sort();
-    format(pkg, json, function() {
+    format(pkg, json, () => {
         console.log('Wrote %s contributors to: %s', result.length, pkg);
     });
 });
